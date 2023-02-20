7a470c5f51e00586cc68afb20e0df15158c716be
Gerald-ux-ux <11geraldmoney@gmail.com>
2023-02-20 13:20:18 +0300

    home header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,12 +6,15 @@ import tw from "tailwind-react-native-classnames";
 const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-[#252E3D] pt-5 ">
+      {/*Header*/}
       <View className="mx-4 mt-4">
         <View className="items-center justify-center flex flex-row  pb-3 mb-3">
           <TouchableOpacity>
             <Bars3Icon size={40} color="#FFFFFF" style={tw`mt-2`} />
           </TouchableOpacity>
-          <Text className="text-center  text-white flex-1 pr-10 ">For riders</Text>
+          <Text className="text-center  text-white underline flex-1 pr-10 pt-1">
+            For riders
+          </Text>
         </View>
       </View>
     </SafeAreaView>
